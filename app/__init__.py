16952ea39923b3902be8c8f1aab72344067121fb
simplg <81249731+simplg@users.noreply.github.com>
2021-06-18 12:30:38 +0200

    init-db

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,23 @@
 from flask import Flask
+from flask_migrate import Migrate
 import json
 
 from app.database.db import db
 from app.controllers import main_controller
 from app.insert_db import insert_db
 
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    if test_config is None:
-        app.config.from_file("config.json", load=json.load)
-    else:
+
+    app.config.from_file("config.json", load=json.load)
+    
+    if test_config is not None:
         app.config.from_mapping(test_config)
 
-    if "default_db" in app.config:
-        dbconfig = app.config[app.config["default_db"]]
+    if "DEFAULT_DB" in app.config:
+        dbconfig = app.config[app.config["DEFAULT_DB"]]
         app.config.from_mapping(
             {
                 "SQLALCHEMY_DATABASE_URI": f"{dbconfig['connector']}://{dbconfig['user']}:{dbconfig['password']}@{dbconfig['host']}:{dbconfig['port']}/{dbconfig['bdd']}",
@@ -24,6 +27,11 @@ def create_app(test_config=None):
 
     # On initialise SQLAlchemy avec les éléments de config à la BDD
     db.init_app(app)
+
+    from app.database.models import Utilisateur, House
+
+    # On initialise l'outil de migration
+    migrate.init_app(app, db)
 
     # On ajoute la commande "flask insert-db" à l'application
     app.cli.add_command(insert_db)
--- app/insert_db.py
+++ app/insert_db.py
@@ -1,4 +1,5 @@
 import click
+import pandas as pd
 from flask.cli import with_appcontext
 from app.database.db import db
 
@@ -6,48 +7,48 @@ from app.database.db import db
 @click.command("insert-db")
 @with_appcontext
 def insert_db():
-    with db.connect() as connexion:
-        with connexion.begin():
-            data_housing = pd.read_csv("housing.csv")
-            data_housing["total_bedrooms"] = data_housing["total_bedrooms"].fillna(
-                data_housing["total_bedrooms"].median()
-            )
-            data_housing[
-                [
-                    "population",
-                    "housing_median_age",
-                    "total_rooms",
-                    "total_bedrooms",
-                    "households",
-                    "median_house_value",
-                ]
-            ] = data_housing[
-                [
-                    "population",
-                    "housing_median_age",
-                    "total_rooms",
-                    "total_bedrooms",
-                    "households",
-                    "median_house_value",
-                ]
-            ].astype(
-                int
-            )
-            data_housing = data_housing.rename(
-                columns={
-                    "longitude": "ho_longitude",
-                    "latitude": "ho_latitude",
-                    "housing_median_age": "ho_housing_median_age",
-                    "total_rooms": "ho_total_rooms",
-                    "total_bedrooms": "ho_total_bedrooms",
-                    "population": "ho_population",
-                    "households": "ho_households",
-                    "median_income": "ho_median_income",
-                    "median_house_value": "ho_median_house_value",
-                    "ocean_proximity": "ho_ocean_proximity",
-                }
-            )
-            data_housing.index += 1
-            data_housing.rename_axis(index="ho_id")
-            data_housing.to_sql("house", if_exists="append", con=connexion, index=False)
+    """Insère les données nécessaire à l'utilisation de l'application
+    """
+    data_housing = pd.read_csv("housing.csv")
+    data_housing["total_bedrooms"] = data_housing["total_bedrooms"].fillna(
+        data_housing["total_bedrooms"].median()
+    )
+    data_housing[
+        [
+            "population",
+            "housing_median_age",
+            "total_rooms",
+            "total_bedrooms",
+            "households",
+            "median_house_value",
+        ]
+    ] = data_housing[
+        [
+            "population",
+            "housing_median_age",
+            "total_rooms",
+            "total_bedrooms",
+            "households",
+            "median_house_value",
+        ]
+    ].astype(
+        int
+    )
+    data_housing = data_housing.rename(
+        columns={
+            "longitude": "ho_longitude",
+            "latitude": "ho_latitude",
+            "housing_median_age": "ho_housing_median_age",
+            "total_rooms": "ho_total_rooms",
+            "total_bedrooms": "ho_total_bedrooms",
+            "population": "ho_population",
+            "households": "ho_households",
+            "median_income": "ho_median_income",
+            "median_house_value": "ho_median_house_value",
+            "ocean_proximity": "ho_ocean_proximity",
+        }
+    )
+    data_housing.index += 1
+    data_housing.rename_axis(index="ho_id")
+    data_housing.to_sql("house", if_exists="append", con=db.engine, index=False)
     print("Tout a été inséré dans la base de données !")
