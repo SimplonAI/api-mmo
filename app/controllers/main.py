48656bfd7906fbd33b480b7f6a77bfb1f1a6619a
simplg <81249731+simplg@users.noreply.github.com>
2021-06-23 13:50:01 +0200

    Ajout du formulaire de login * Fix des problemes avec le login * Ajout du bouton remember_me * Répare un probleme avec le manifest * Remplace bootstrap.min.js par bundle

--- app/controllers/main.py
+++ app/controllers/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, flash, redirect, request, url_for
 from flask_login import login_required, login_user
+from flask_login.utils import logout_user
 from werkzeug.security import check_password_hash
 from urllib.parse import urlparse, urljoin
 
@@ -27,13 +28,21 @@ def login():
     """
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        user: User = User.query.filter_by(email=login_form.email).first()
-        if user is not None and check_password_hash(user.password, login_form.password):
-            login_user(user)
-            next = request.form.get("next")
-            if is_safe_url(next):
+        user: User = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and check_password_hash(user.password, login_form.password.data):
+            login_user(user, remember=login_form.remember_me.data)
+            next = request.args.get("next")
+            if next is not None and is_safe_url(next):
                 return redirect(next)
             return redirect(url_for("main.index"))
         else:
             flash("Le mot de passe ou l'adresse e-mail ne correspond pas à un utilisateur", category="error")
-    return render_template("login.html")
+    return render_template("login.html", login_form=login_form)
+
+@main_blueprint.route("/logout", methods=["GET", "POST"])
+@login_required
+def logout():
+    """Controller pour déconnecter un utilisateur
+    """
+    logout_user()
+    return redirect(url_for("main.login"))
