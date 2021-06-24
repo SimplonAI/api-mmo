74d307e258a51cda74d1b7aba8b18d3c4f3351f8
anthony-marais <marais.anthony.pro@gmail.com>
2021-06-24 16:27:36 +0200

    ajout des fichier liée au la suppression des logement

--- app/controllers/api.py
+++ app/controllers/api.py
@@ -1,15 +1,19 @@
 from io import BytesIO
-from flask import Blueprint, Response, abort
+from flask import Blueprint, Response, abort, abort, flash, redirect, render_template, url_for
+
+from flask_login import login_required
 import pandas as pd
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from app.forms import DashboardForm
 from app.services import plot_manager
 from app.db import db
 from app.utils import house_results_to_dataframe
-
+from app.models import House
 api_blueprint = Blueprint('api', __name__, url_prefix="/api")
 
+
 @api_blueprint.route("/plot/<name>", methods=["GET"])
+@login_required
 def plot(name):
     if name in plot_manager:
         houses = pd.read_sql("SELECT * FROM house", db.engine)
@@ -19,3 +23,18 @@ def plot(name):
         FigureCanvasAgg(fig).print_png(png)
         return Response(png.getvalue(), mimetype='image/png')
     abort(404)
+
+
+@api_blueprint.route("/list_houses/delete/<int:id>", methods=["GET","POST"])
+@login_required
+def delete_house():
+    house = House.query.get_or_404(id)
+    db.session.delete(house)
+    db.session.commit()
+    flash('You have successfully deleted the department.')
+
+    # redirect to the departments page
+    return redirect(url_for('list_houses'))
+
+
+    return render_template(title="Delete Department")
