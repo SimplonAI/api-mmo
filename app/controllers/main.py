c8414a28a245fa0ed458f5efa376a824171d51fd
anthony-marais <marais.anthony.pro@gmail.com>
2021-06-24 14:17:16 +0200

    Ajout du controller et du template html pour la liste des maison

--- app/controllers/main.py
+++ app/controllers/main.py
@@ -3,46 +3,76 @@ from flask_login import login_required, login_user
 from flask_login.utils import logout_user
 from werkzeug.security import check_password_hash
 from urllib.parse import urlparse, urljoin
-
+import math
 from app.forms import LoginForm
-from app.models import User
+from app.models import User, House
+
 
 def is_safe_url(target):
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
     return test_url.scheme in ("http", "https") and ref_url.netloc == test_url.netloc
 
+
 # On crée une blueprint qui contiendra toutes les routes/controllers pour les urls de haut niveau ("/", "/login", "/logout"; "/predict")
-main_blueprint = Blueprint('main', __name__, url_prefix="/")
+main_blueprint = Blueprint("main", __name__, url_prefix="/")
+
 
 @main_blueprint.route("/")
 @login_required
 def index():
-    """Controller pour le dashboard
-    """
+    """Controller pour le dashboard"""
     return "Hello World ! Agile"
+
 
 @main_blueprint.route("/login", methods=["GET", "POST"])
 def login():
-    """Controller pour la connexion de l'utilisateur
-    """
+    """Controller pour la connexion de l'utilisateur"""
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user: User = User.query.filter_by(email=login_form.email.data).first()
-        if user is not None and check_password_hash(user.password, login_form.password.data):
+        if user is not None and check_password_hash(
+            user.password, login_form.password.data
+        ):
             login_user(user, remember=login_form.remember_me.data)
             next = request.args.get("next")
             if next is not None and is_safe_url(next):
                 return redirect(next)
             return redirect(url_for("main.index"))
         else:
-            flash("Le mot de passe ou l'adresse e-mail ne correspond pas à un utilisateur", category="error")
+            flash(
+                "Le mot de passe ou l'adresse e-mail ne correspond pas à un utilisateur",
+                category="error",
+            )
     return render_template("login.html", login_form=login_form)
+
 
 @main_blueprint.route("/logout", methods=["GET", "POST"])
 @login_required
 def logout():
-    """Controller pour déconnecter un utilisateur
-    """
+    """Controller pour déconnecter un utilisateur"""
     logout_user()
     return redirect(url_for("main.login"))
+
+
+@main_blueprint.route("/list_houses", methods=["GET", "POST"])
+@login_required
+def list_houses():
+    """Controller pour afficher la liste des logements"""
+    page = request.args.get("p", 1)
+    try:
+        page = int(page)
+    except ValueError:
+        page = 1
+
+    count_page = House.query.count()
+
+    list_housing = House.query.limit(25).offset(25 * (page - 1)).all()
+
+    return render_template(
+        "list_houses.html",
+        list_housing=list_housing,
+        count_page=math.ceil(count_page / 25),
+        current_page=page,
+        title="list_housing",
+    )
