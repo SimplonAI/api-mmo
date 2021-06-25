b2a388bcec633c61de427989295f54b03509e6d7
anthony-marais <marais.anthony.pro@gmail.com>
2021-06-25 10:11:01 +0200

    update delete logement controller and template

--- app/controllers/api.py
+++ app/controllers/api.py
@@ -1,5 +1,14 @@
 from io import BytesIO
-from flask import Blueprint, Response, abort, abort, flash, redirect, render_template, url_for
+from flask import (
+    Blueprint,
+    Response,
+    abort,
+    abort,
+    flash,
+    redirect,
+    render_template,
+    url_for,
+)
 
 from flask_login import login_required
 import pandas as pd
@@ -9,7 +18,8 @@ from app.services import plot_manager
 from app.db import db
 from app.utils import house_results_to_dataframe
 from app.models import House
-api_blueprint = Blueprint('api', __name__, url_prefix="/api")
+
+api_blueprint = Blueprint("api", __name__, url_prefix="/api")
 
 
 @api_blueprint.route("/plot/<name>", methods=["GET"])
@@ -21,20 +31,20 @@ def plot(name):
         fig = plot_manager[name].plot(houses)
         png = BytesIO()
         FigureCanvasAgg(fig).print_png(png)
-        return Response(png.getvalue(), mimetype='image/png')
+        return Response(png.getvalue(), mimetype="image/png")
     abort(404)
 
 
-@api_blueprint.route("/list_houses/delete/<int:id>", methods=["GET","POST"])
+@api_blueprint.route("/list_houses/delete/<int:id>", methods=["GET", "POST"])
 @login_required
-def delete_house():
+def delete_house(id):
     house = House.query.get_or_404(id)
     db.session.delete(house)
     db.session.commit()
-    flash('You have successfully deleted the department.')
+    flash("You have successfully deleted the house.", "info")
 
     # redirect to the departments page
-    return redirect(url_for('list_houses'))
+    return redirect(url_for("main.list_houses"))
 
 
-    return render_template(title="Delete Department")
+#    return render_template(title="Delete Department")
