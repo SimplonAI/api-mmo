92dc1e6832104d43790aa5521f3030dc5bf9ca10
akuma06 <demon.akuma06@gmail.com>
2021-06-28 08:20:36 +0200

    Prepare gunicorn pour deploiement sur heroku

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -5,6 +5,8 @@ echo "Waiting for Postgres (10 seconds)..."
 sleep 10
 
 python3.9 -m flask db upgrade
-python3.9 -m flask insert-db
-
-python3.9 -m flask run
+if [ ! -f ".db-initialized" ]; then
+    python3.9 -m flask insert-db
+    echo "initialized" > .db-initialized
+fi
+python3.9 -m gunicorn --workers=2 'app:create_app()' -b :5000
