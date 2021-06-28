edee638b0020663dd0db020968f1eef49f663d6f
simplg <81249731+simplg@users.noreply.github.com>
2021-06-28 15:21:55 +0200

    still for heroku

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -6,7 +6,7 @@ sleep 10
 
 python3 -m flask db upgrade
 if [ ! -f ".db-initialized" ]; then
-    python -m flask insert-db
+    python3 -m flask insert-db
     echo "initialized" > .db-initialized
 fi
 python3 -m gunicorn --workers=2 'app:create_app()' -b :5000
