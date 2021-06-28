a92fd1d722adad25e7d56d103d0f8d58d7e049a3
simplg <81249731+simplg@users.noreply.github.com>
2021-06-28 15:21:43 +0200

    still for heroku

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 echo "Waiting for Postgres (10 seconds)..."
 sleep 10
 
-python -m flask db upgrade
+python3 -m flask db upgrade
 if [ ! -f ".db-initialized" ]; then
     python -m flask insert-db
     echo "initialized" > .db-initialized
 fi
-python -m gunicorn --workers=2 'app:create_app()' -b :5000
+python3 -m gunicorn --workers=2 'app:create_app()' -b :5000
