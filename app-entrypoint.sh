d33af0986a481195889f6dafc984996d538d3bae
simplg <81249731+simplg@users.noreply.github.com>
2021-06-28 15:38:27 +0200

    Revert "still for heroku"
    This reverts commit a99f7a4d899f9ed44c59baf0414bf1061b93b764.

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 echo "Waiting for Postgres (10 seconds)..."
 sleep 10
 
-python3 -m flask db upgrade
+python3.9 -m flask db upgrade
 if [ ! -f ".db-initialized" ]; then
-    python3 -m flask insert-db
+    python3.9 -m flask insert-db
     echo "initialized" > .db-initialized
 fi
-python3 -m gunicorn --workers=2 'app:create_app()' -b :5000
+python3.9 -m gunicorn --workers=2 'app:create_app()' -b :5000
